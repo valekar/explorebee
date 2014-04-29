ef4d53b06a0d60dce1dba039757234f380637864
srini <srinivas.valekar@gmail.com>
2014-04-29 10:43:01 +0530

    background

--- app/assets/javascripts/masonry.js
+++ app/assets/javascripts/masonry.js
@@ -3,9 +3,14 @@ $('#masonry-container').masonry({
     // set columnWidth a fraction of the container width
     columnWidth: function( containerWidth ) {
         return containerWidth /6;
+    },
+
+    isAnimated: false,
+    animationOptions: {
+        duration: 750,
+        easing: 'linear',
+        queue: false
     }
-
-
 
 });
 
