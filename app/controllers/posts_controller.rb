105cdafe618c30e577512397be06906606688b33
srini <srinivas.valekar@gmail.com>
2014-05-18 19:06:24 +0530

    sanitize

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -67,6 +67,9 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
+
+
+
     @post.destroy
     respond_to do |format|
       format.html { redirect_to posts_url }
